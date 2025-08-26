61f2182adc1438099a183115c077b763134ac75d
Ethan E. <eastwoode375@gmail.com>
2025-08-25 22:32:51 -0500

    Move Repo to Fork

---
+++ EcommerceAPI.GoldRino456/EcommerceAPIDemo/Data/DTOs/SaleDto.cs
@@ -0,0 +1,13 @@
+﻿using EcommerceAPIDemo.Data.Models;
+
+namespace EcommerceAPIDemo.Data.DTOs;
+
+public class SaleDto
+{
+    public List<int>? PurchasedGameIds { get; set; }
+    public CreditCardTypes creditCardType { get; set; }
+    public int LastFourDigitsOfPaymentCard { get; set; }
+    public double SubTotal { get; set; }
+    public double SalesTax { get; set; }
+    public double Total { get; set; }
+}
